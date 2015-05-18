d9d8c4f8ee6e72695fc1290f8e8deea083e16967
thanhcao <thanh.cao@big-labs.com>
2015-05-18 09:47:26 +0700

    Add code

--- app.js
+++ app.js
@@ -3,16 +3,37 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+var storeMessage = function(name, data){
+	var message = JSON.stringify({name:name,data:data});
+	redisClient.lpush("messages",message,function(err,res){
+		redisClient.ltrim("messages",0,9);
+	});
+}
+
 io.on('connection',function(client){
 	console.log('Client connected');
 	client.on('messages',function(data){ // when server receive message
 		var nickname = client.nickname;
 		client.broadcast.emit("messages",nickname+":"+data);
 		client.emit("messages",nickname+":"+data);
+		storeMessage(nickname,data)
 	});
 	client.on('join',function(name){ // when server receive message
 		console.log("New user: "+name+" join");
-		client.nickname = name;
+		redisClient.lrange("messages",0,-1,function(err,messages){
+			messages = messages.reverse();
+			client.nickname = name;
+			messages.forEach(function(message){
+				message = JSON.parse(message);
+				client.emit('messages',message.name + ": " + message.data);
+			});
+		});
+
+		client.broadcast
+		redisClient.sadd("chatters",name)
 	});
 });
 
